ae1b3a1e9276e512f2998c080a753bcd767b356e
StanleyMurigi <stannjoroge643@gmail.com>
2024-07-24 23:40:20 +0300

    promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Resolve the promise for now, you can add more logic here if needed
+      resolve();
+    });
+  }
+  
