56e2083b805abfdf911306edca1929d9e842fec6
StanleyMurigi <stannjoroge643@gmail.com>
2024-07-25 22:57:23 +0300

    implementing a class

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+// 6-sky_high.js
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Number of floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
+
