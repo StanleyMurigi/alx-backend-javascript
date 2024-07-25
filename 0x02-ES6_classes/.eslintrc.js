f51f30a37be90f9bec86f0858f60255ba758f894
StanleyMurigi <stannjoroge643@gmail.com>
2024-07-25 23:03:40 +0300

    config files

---
+++ 0x02-ES6_classes/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+};
+
